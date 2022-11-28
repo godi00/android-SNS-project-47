a193b0795b25a9ec85dcb0f8590c2f5b9e52600d
godi00 <sntsus2lu@gmail.com>
2022-11-28 15:58:09 +0900

    ADD BottomNavigation by qkrtg

---
+++ app/src/main/java/com/example/androidsnsproject/NavigationActivity.kt
@@ -0,0 +1,47 @@
+package com.example.androidsnsproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidsnsproject.databinding.ActivityNavigationBinding
+
+class NavigationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNavigationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNavigationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        
+
+        binding.navigationView.setOnItemSelectedListener {
+            item -> when(item.itemId) {
+                R.id.homeFragment -> {
+                    // move to homeFragment
+                    val homeFrag = HomeFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.mainFrameLayout, homeFrag).commit()
+                    true
+                }
+                R.id.friendFragment -> {
+                    // move to friendFragment
+                    val friendFrag = FriendFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.mainFrameLayout, friendFrag).commit()
+                    true
+                }
+                R.id.profileFragment -> {
+                    // move to profileFragment
+                    val profileFrag = ProfileFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.mainFrameLayout, profileFrag).commit()
+                    true
+                }
+                R.id.notifyFragment -> {
+                    // move to notifyFragment
+                    val notifyFrag = NotifyFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.mainFrameLayout, notifyFrag).commit()
+                    true
+                }
+            else -> { false }
+            }
+        }
+    }
+}
